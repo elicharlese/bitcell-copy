43c52284bdc59c9302621b48df91886127eaa778
Elias Estrada <elicharles.e@gmail.com>
2022-04-29 16:44:02 -0700

    Adding market makers functions

---
+++ packages/dapplib/contracts/market_makers/selling-climax/selling-climax.sol
@@ -0,0 +1,35 @@
+pragma solidity ^0.6.0
+
+contract Selling_Climax_Goal {
+    function get_psb_selling_climax() public {
+    //Get PSB Climax signal
+    }
+    
+    function set_selling_climax_goal() private {
+    //Set Climax Goal
+    }
+
+    function set_selling_climax_boundary() private {
+    //Set Selling Climax Boundary
+    }
+}
+
+contract Selling_Climaxation {
+    function increase_selling_volume() private {
+    //Increase Selling Volume
+    }
+
+    function repeat_increase_selling_volume() private {
+    //Repeat Increase Selling Volume
+    }
+}
+
+contract Selling_Climax_Finish {
+    function finish_selling_climax() private {
+    //Finish Buying Climax
+    }
+
+    function begin_testing_supply() private {
+    //Begin Testing Supply
+    }
+}
