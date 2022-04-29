b7d67d6618c18fbedc576a5ca9d79e123f5465be
Elias Estrada <elicharles.e@gmail.com>
2022-04-28 18:50:16 -0700

    adding pages to dapp maybe?

--- packages/client/src/pages/calculator.js
+++ packages/client/src/pages/calculator.js
@@ -34,7 +34,7 @@ render() {
 let content = html`
 <div class="container m-auto">
 <div class="row fadeIn mt-3 p-2 block">
-    <h2 class="text-6xl">🎉 Dappiness!</h2>
+    <h2 class="text-6xl">Calculator</h2>
     <p class="mt-3">
     Your Dapp is ready, and the world is waiting for you to create
     something amazing.
