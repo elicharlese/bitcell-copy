246d59e495d357e321034e58174272f9c5e5c365
Elias Estrada <elicharles.e@gmail.com>
2022-04-29 14:38:44 -0700

    Migrating and Merging Repos

---
+++ packages/dapplib/contracts/address_migrations/address_migrations.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.6.0
+
+contract Begin_Migrations {
+    function grab_addresses() private {
+        //Grab the Addresses
+    }
+    function assign_new_addresses() private {
+        //Pick new Addresses
+    }
+    function update_new_address() private {
+        //Update new addresses
+    }
+    function return_addresses() private {
+        //Return new addresses
+    }
+}
+
+contract Test_Migrations {
+    function {
+        //
+    }
+}
+
+contract Finish_Migrations {
+    function {
+        //
+    }
+}
+
+contract Check_Migrations_Security {
+    function migration_pen_test() private{
+        //
+    }
+}
---
+++ packages/dapplib/contracts/auto/margin_meter.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.0
+
+contract Define_Margin_Level {
+    function select_blockchain() {
+        
+    }
+    function query_blockchain() {
+        //Query the blockchain margin performace and level of margin traders. 
+    }
+    function track_traders() {
+        //Track the perpetual futures traders
+    }
+    function define_margin_level() {
+
+    }
+    function alert_strategy_changer() {
+
+    }
+}
+
+contract Execute_Margin_Strategy {
+    function {
+
+    }
+}
