0bfcbd61129d14c5a61e03a2cc1cdd7f074315a9
Elias Estrada <elicharles.e@gmail.com>
2022-05-05 23:58:34 -0700

    Adding a few things

---
+++ packages/dapplib/contracts/address_migrations/address_storage.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.0
+
+contract Store_Addresses {
+    function store_new_address() public {
+
+    }
+}
+
+contract Search_Addresses {
+//Will comb through the historical record of addresses to see what has been used in the past and whem
+    function search_address_bank() {
+        if address = current_address {
+            msg.sender["Found it:" current_address]
+        }
+    }
+}
