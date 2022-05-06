49fdcae21b2e0e517d73e710d4b6a5cb88ee711e
Elias Estrada <elicharles.e@gmail.com>
2022-05-06 16:12:38 -0700

    Working on balancers and farmers

---
+++ packages/workspace/balancers/bal.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.0
+
+contract Manage_Bal_Balance {
+    function grab_bal_balance() {
+        //We will need to retrieve the bal balance so we can compare it in the next step
+    }
+    function check_against_bal_goal() {
+        //Retrieve bal goal
+        //Calculate difference
+    }
+    function restore_balance_in_bal() {
+        //Correct the amount of bal in storage
+    }
+}
---
+++ packages/workspace/farmers/barn.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.0
+
+contract Manage_Barn_Balance {
+    function grab_barn_balance() {
+        //We will need to retrieve the barn balance so we can compare it in the next step
+    }
+    function check_against_barn_goal() {
+        //Retrieve barn goal
+        //Calculate difference
+    }
+    function restore_balance_in_barn() {
+        //Correct the amount of barn in storage
+    }
+}
