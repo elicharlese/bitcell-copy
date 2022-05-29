33094519f57ef4c7baabbd069a6a040afa72a009
elicharlese <elicharles.e@gmail.com>
2022-05-29 13:18:37 -0700

    Main flashloan template

--- packages/dapplib/contracts/arbitrage_bots/flashloan-arbitrage-bot-main/flashloan-arbitrage-bot-main/FlashArbitrageTrader.sol
+++ packages/dapplib/contracts/arbitrage_bots/flashloan-arbitrage-bot-main/flashloan-arbitrage-bot-main/FlashArbitrageTrader.sol
@@ -7,7 +7,6 @@ pragma solidity 0.6.12;
     - DAI:                                  0xf80A32A835F79D7787E8a8ee5721D0fEaFd78108
     - ETH:                                  0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE
     - Aave LendingPoolAddressesProvider:    0x1c8756FD2B28e9426CDBDcC7E3c4d64fa9A54728
-    
     Mainnet instances:
     - Uniswap V2 Router:                    0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D
     - Sushiswap V1 Router:                  0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F
