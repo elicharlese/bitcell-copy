8e0aef5d754d22bc701082450d4b5f8d22a266d4
elicharlese <elicharles.e@gmail.com>
2022-05-29 13:20:24 -0700

    Organizing

---
+++ packages/dapplib/contracts/arbitrage_bots/flashloan-by-market-maker/vpa/distribution/distribution.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.6.0
+
+contract Distribution_Goal {
+    function take_inventory() private {
+    //Take Inventory
+    }
+
+    function set_warehouse_goal() private {
+    //Set Warehouse goal
+    }
+
+    function start_accumalation_phase() private {
+    //Start Accumulation phase
+    }
+}
+
+contract Distribution_Shake {
+    function shake_success() private {
+    //Shake Success
+    }
+
+    function shake_error() private {
+    //Shake Error
+    }
+}
+
+contract Distribution_Test {
+    function test_initiation() private {
+    //Test Initiation
+    }
+
+    function test_function() private {
+    //Test Functionality
+    }
+
+    function test_success() private {
+    //Test Success
+    }
+
+    function test_error() private {
+    //Test Error
+    }
+}
+
+contract Distribution_Finish {
+    function finish_distribution() private {
+    //Begin Finishing Distribution
+    }
+    function start_buying_climax() {
+    //Start Buying Climax
+    }
+}
